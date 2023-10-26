c01f3cfdc288faeae2a68e5a5b620bd21cec6d3a
antonioagnt <2728086+antonioagnt@users.noreply.github.com>
2023-10-26 17:43:37 -0300

    Week 2 Semantic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import React from 'react';
 
 function App() {
-  return;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
